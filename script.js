04d4b5a4f9179996727aab7feec2ea2d95a8df79
MtendeKuyokwa <mtendekuyokwa19@gmail.com>
2023-12-28 09:06:26 +0200

    creating of the menu

--- script.js
+++ script.js
@@ -1,111 +1,47 @@
-let parent=document.querySelector('#container')
-let grid=document.querySelector('.grid')
-let noGrid=document.querySelector('.noGrid')
-let black=document.querySelector('.black')
-let red=document.querySelector('.red')
-let green=document.querySelector('.green')
-let boxNum=document.querySelector('.boxSize')
-let size;
-let measurement;
-   do{
+/* eslint-disable no-unused-vars */
+/* eslint no-console: "error" */
+const menuFunctionalities = (() => {
+  const menuButton = document.querySelector('#menubtn')
+  const menuList = document.querySelector('#ListItems')
+  const buttonStatus = {
+    status: false
+  }
+  function showMenuItems () {
+    menuList.className = 'showItems'
+    buttonStatus.status = true
+  }
 
-           
-         measurement=function(){
-            size=prompt("enter box sizes between 17-99")
-            
-        }
-       measurement();
-       
+  function hideMenuItems () {
+    menuList.className = 'ListItems'
+    buttonStatus.status = false
+  }
 
-   }while(!(size>16&&size<100))   
-let chosenColor=function (x){
-  
-    switch(x){
-        case 1:
-            color="black"
-            console.log ("black")
-            break;
-        case 2:
-            color="red"
-            console.log ("red")
-            break;
-        case 3:
-            color="green"
-            console.log ("green")
-            break;       
-        default:
-            color="blue"
-
+  function determiningToogle () {
+    if (buttonStatus.status === false) {
+      showMenuItems()
+      changeToClosingicon()
+      changeBackgroundToBlack()
+    } else if (buttonStatus.status === true) {
+      hideMenuItems()
+      changeToHamburgerIcon()
+      changeBackgroundToblue()
     }
-}
+  }
 
-chosenColor(2)
-black.addEventListener('click',function(e){
+  function changeToClosingicon () {
+    const hamburgerMenu = document.querySelector('#menubtn>svg')
 
-    chosenColor(1);
+    menuButton.textContent = 'X'
+  }
+  function changeToHamburgerIcon () {
+    menuButton.textContent = '≡'
+  }
+  function changeBackgroundToBlack () {
+    document.body.style.setProperty('background-color', '#050505')
+  }
+  function changeBackgroundToblue () {
+    document.body.style.setProperty('background-color', '#070D2D')
+  }
 
-})
-red.addEventListener('click',function(e){
-
-
-    chosenColor(2)
-})
-
-green.addEventListener('click',function(e){
-
-
-    chosenColor(3)
-})
-
-function container(){
-
- let firstInnerCointainer=document.createElement('div')
- firstInnerCointainer.classList.add('div')
-    parent.appendChild(firstInnerCointainer)
-
-console.log("first container created")
-let i=0;
-while (i<=size){
-i++
-let secondInnercontainer=document.createElement('div')
-secondInnercontainer.classList.add('divIn')
-firstInnerCointainer.appendChild(secondInnercontainer)
-let secondStyle=secondInnercontainer.style;
-
-grid.addEventListener('click',function(e){
-
-
-secondStyle.setProperty("border","solid black 0.1px")
-
-})
-noGrid.addEventListener('click',function(e){
-
-    secondStyle.setProperty("border","solid black 0px")
-    
-    })
-
-secondInnercontainer.addEventListener('mouseover',function(e){
-    
-        secondStyle.setProperty("background-color",color)
-})
-
-let erase=document.querySelector('.erase')
-erase.addEventListener('click',function(e){
-
-    secondStyle.setProperty("background-color","white")
-
-})
-
-
-}
-
-}
-
-for(i=0;i<size;i++){
-    container();
-
-
-    console.log("yandhi");
-    
-            }   
-        
+  menuButton.addEventListener('click', determiningToogle)
+})()
